a74f09c7bfce762462f3a2170136ffd72afc6e97
hrith <hrith@DESKTOP-MNCUUEH>
2022-12-16 17:51:14 +0530

    Push AGAIN

--- Multiplex/src/test/java/com/multiplex/MultiplexApplicationTests.java
+++ Multiplex/src/test/java/com/multiplex/MultiplexApplicationTests.java
@@ -1,13 +1,36 @@
 package com.multiplex;
 
+ 
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+ 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+ 
+
+import com.multiplex.entities.User;
+import com.multiplex.repository.UserRepository;
+
+ 
+
 @SpringBootTest
-class MultiplexApplicationTests {
+class MultiplexProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+ 
 
+     @Autowired
+        private UserRepository userRespository;
+
+ 
+
+        @Test
+        public void addUser(){
+            User user=new User(2, "Shubham", "7898456786", "shubh.am@gmail.com", "Password");
+            userRespository.save(user);
+            assertNotNull(userRespository.findById(2).get());
+        }
 }
